a49101585d07eab3482d39a4915bcba708c7a0dd
Blake <tk0wo3@gmail.com>
2023-02-15 22:42:57 +0800

    Add Solana package

---
+++ blockchain/Solana/Solana.go
@@ -0,0 +1,19 @@
+package Solana
+
+type SolWalletManager struct{}
+
+func (swm *SolWalletManager) CreateWallet() (_address string, _privateKey string, _err error) {
+
+}
+
+func (swm *SolWalletManager) GetBalance(_address string) (balance float64, err error) {
+	return 0.0, nil
+}
+
+func (swm *SolWalletManager) SendTransaction(_fromAddress string, _toAddress string, _amount float64) (txHash string, err error) {
+	return "", nil
+}
+
+func (swm *SolWalletManager) DestroyWallet(_address string) (err error) {
+	return nil
+}
